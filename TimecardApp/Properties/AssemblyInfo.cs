2e70869f55c5559af4bb339323ea7f8446bfbe10
Kn0rk3 <georg.leuschel@gmail.com>
2014-09-23 00:02:35 +0200

    assembly changes

--- TimecardApp/Properties/AssemblyInfo.cs
+++ TimecardApp/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Resources;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DellApp")]
+[assembly: AssemblyProduct("TimecardApp")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
